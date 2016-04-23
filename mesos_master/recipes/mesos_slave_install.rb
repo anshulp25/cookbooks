f992d03eaf3be2ae35e9298aced7c499fd2d1b89
Anshul Patel <anshulpatel25@gmail.com>
2016-04-23 18:25:13 +0530

    Updating slave install

--- mesos_master/recipes/mesos_slave_install.rb
+++ mesos_master/recipes/mesos_slave_install.rb
@@ -1,8 +1,12 @@
-package "MesosRepo" do
-  source  node['mesos']['repourl']
-  action  :install
+remote_file "tmp/mesosrepo.rpm" do
+  source node['mesos']['repourl']
 end
 
+
+package "MesosRepo" do
+  source  "/tmp/mesosrepo.rpm"
+  action  :install
+end
 
 package "mesos" do
   action :install
